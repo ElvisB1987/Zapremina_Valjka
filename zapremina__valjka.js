51fead51984613783a68764e7a99b08890b0f3d5
Elvis <ebalija@gmail.com>
2021-03-06 06:15:46 +0100

    Prvi

---
+++ zapremina__valjka.js
@@ -0,0 +1,7 @@
+var r = parseInt(prompt("Unesite velicinu poluprecnika r :"));
+var H = parseInt(prompt("Unesite Visinu Valjka H :"));
+const Pi = 3.14;
+
+V = ((r * r) * Pi) * H;
+
+document.write("<h1>Zapremina Valjaka je:<h1>" + V);
